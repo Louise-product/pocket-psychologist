64b38ca228127ea128eed6f396da1e3db4a3dc57
Enitan Soyemi <eb.soyemi@gmail.com>
2025-11-25 16:53:04 +0100

    add validations

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,5 +1,9 @@
 class Chat < ApplicationRecord
+
+  validates :user_id, presence: true
+  validates :problem_id, presence: true
   belongs_to :problem
   belongs_to :user
   has_many :messages, dependent: :destroy
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # validates :gender, presence: true, inclusion: { in: %w[male, female, non-binary, other] }
+  # validates :age, presence: true, numericality: { only_integer: true }
 end
+

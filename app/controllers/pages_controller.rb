67f5cd4f285cb5df4b93dfb65d1171060deaedc6
antonio_sartor <Antoniosartor275@gmail.com>
2025-11-26 15:06:30 +0100

    first dashboard scratch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,12 @@ class PagesController < ApplicationController
 
   def dashboard
     @problems = current_user.problems
+    # @chats = current_user.problems.chats
+    # @messages = current_user.problems.chats.messages
+    # @chats.each do |chat|
+    #   chat.title = @messages.first
+    #   @title = chat.title 
+    # end
   end
 
 end

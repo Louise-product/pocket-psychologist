bc07e62937ade4b6639b988f88d5956b978cf9a3
antonio_sartor <Antoniosartor275@gmail.com>
2025-11-27 12:22:03 +0100

    sidebar

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,8 @@ class Message < ApplicationRecord
   validates :chat_id, presence: true, numericality: { only_integer: true }
   validates :role, presence: true, inclusion: { in: ["user", "assistant"] }
   validates :content, presence: true
+
+  scope :by_user, -> { where(role: "user") }
+  scope :by_assistant, -> { where(role: "assistant") }
+
 end

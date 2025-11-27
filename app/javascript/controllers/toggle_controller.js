5436c14e15bcfbdeab20b7488dbfd80ae929a4b6
antonio_sartor <Antoniosartor275@gmail.com>
2025-11-27 16:11:18 +0100

    toogle with the correct flashes

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -6,10 +6,13 @@ export default class extends Controller {
 
   call(event) {
     event.preventDefault()
-    if (this.hideableTarget && this.hideableTarget.classList.contains("d-none")) {
+    console.log("toggle!", this.showTarget)
+    console.log(this.hideableTarget && this.hideableTarget.classList.contains("d-none"))
+    console.log(this.showTarget && this.showTarget.classList.contains("d-none"))
+    if (this.hideableTarget.classList.contains("d-none")) {
       this.hideableTarget.classList.remove("d-none")
       this.showTarget.classList.add("d-none")
-    } else if (this.showTarget && this.showTarget.classList.contains("d-none")){
+    } else {
       this.hideableTarget.classList.add("d-none")
       this.showTarget.classList.remove("d-none")
     }
